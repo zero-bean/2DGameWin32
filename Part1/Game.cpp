a4d2ab0555b1b751e6d182858c64c43466ebd9a9
YEONGBIN PAK <suplem07@naver.com>
2024-10-01 18:23:08 +0900

    Added InputManager and TimeManager

--- Part1/Game.cpp
+++ Part1/Game.cpp
@@ -16,11 +16,13 @@ void Game::Init(HWND hwnd)
 	_hwnd = hwnd;
 	_hdc = ::GetDC(hwnd);
 	GET_SINGLE(TimeManager)->Init();
+	GET_SINGLE(InputManager)->Init(hwnd);
 }
 
 void Game::Update()
 {
 	GET_SINGLE(TimeManager)->Update();
+	GET_SINGLE(InputManager)->Update();
 }
 
 void Game::Render()
@@ -28,8 +30,16 @@ void Game::Render()
 	uint32 fps = GET_SINGLE(TimeManager)->GetFps();
 	float deltatime = GET_SINGLE(TimeManager)->GetDeltaTime();
 
-	wstring str = std::format(L"FPS({0}), DT({1} ms)", fps, static_cast<int32>(deltatime));
-	::TextOut(_hdc, 650, 10, str.c_str(), static_cast<int32>(str.size()));
+	{
+		POINT mousePos = GET_SINGLE(InputManager)->GetMousePos();
+		wstring str = std::format(L"Mouse({0}, {1})", mousePos.x, mousePos.y);
+		::TextOut(_hdc, 20, 10, str.c_str(), static_cast<int32>(str.size()));
+	}
+
+	{
+		wstring str = std::format(L"FPS({0}), DT({1} ms)", fps, static_cast<int32>(deltatime));
+		::TextOut(_hdc, 650, 10, str.c_str(), static_cast<int32>(str.size()));
+	}
 
 	::Rectangle(_hdc, 200, 200, 400, 400);
 }
--- Part1/Part1.cpp
+++ Part1/Part1.cpp
@@ -34,6 +34,7 @@ int APIENTRY wWinMain(_In_ HINSTANCE hInstance,
     game.Init(g_hWnd);
 
     MSG msg = {};
+    uint64 prevTick = 0;
 
     // 3) 메인 루프
     while (msg.message != WM_QUIT)
@@ -43,9 +44,16 @@ int APIENTRY wWinMain(_In_ HINSTANCE hInstance,
             ::DispatchMessage(&msg);
         }
         else {
-            // 게임
-            game.Update();
-            game.Render();
+            uint64 now = ::GetTickCount64();
+
+            if (now - prevTick >= 10) {
+                // 게임
+                game.Update();
+                game.Render();
+
+                prevTick = now;
+            }
+
         }
     }
 
